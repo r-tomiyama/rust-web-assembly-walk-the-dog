a871aa44417512cdcca3220e9439eca636dbd5a3
r-tomiyama <peko858.ab@gmail.com>
2023-07-25 18:25:07 +0900

    init

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+use wasm_bindgen::prelude::*;
+use web_sys::console;
+use wasm_bindgen::JsCast;
+
+
+// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
+// allocator.
+//
+// If you don't want to use `wee_alloc`, you can safely delete this.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+
+// This is like the `main` function, except for JavaScript.
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+    console_error_panic_hook::set_once();
+
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let canvas = document.get_element_by_id("canvas").unwrap().dyn_into::<web_sys::HtmlCanvasElement>().unwrap();
+
+    let context = canvas.get_context("2d").unwrap().unwrap().dyn_into::<web_sys::CanvasRenderingContext2d>().unwrap();
+
+    context.move_to(300.0, 0.0); // top of triangle
+
+    context.begin_path();
+    context.line_to(0.0, 600.0);
+    context.line_to(600.0, 600.0);
+    context.line_to(300.0, 0.0);
+    context.close_path();
+    context.stroke();
+    context.fill();
+
+    console::log_1(&JsValue::from_str("Hello world!"));
+
+    Ok(())
+}
