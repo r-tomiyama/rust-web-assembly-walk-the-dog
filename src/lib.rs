79590938288e663f8e65b3dd33eb17844e8ab600
r-tomiyama <peko858.ab@gmail.com>
2023-07-26 14:35:22 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ use wasm_bindgen::prelude::*;
 use web_sys::console;
 use wasm_bindgen::JsCast;
 use rand::prelude::*;
+use std::rc::Rc;
+use std::sync::Mutex;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -23,7 +25,39 @@ pub fn main_js() -> Result<(), JsValue> {
 
     let context = canvas.get_context("2d").unwrap().unwrap().dyn_into::<web_sys::CanvasRenderingContext2d>().unwrap();
 
-    sierpinski(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)], (0, 255, 0), 5);
+    wasm_bindgen_futures::spawn_local(async move {
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
+        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+        let error_tx = Rc::clone(&success_tx);
+
+        let callback = Closure::once(move || {
+            if let Some(success_tx) =
+                success_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                        console::log_1(&JsValue::from_str("Image loaded!"));
+                        success_tx.send(Ok(()));
+                    };
+        });
+        let error_callback = Closure::once(move |err| {
+            if let Some(error_tx) =
+                error_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                        console::error_1(&JsValue::from_str("error"));
+                        error_tx.send(Err(err));
+                }
+        });
+
+        let image: web_sys::HtmlImageElement = web_sys::HtmlImageElement::new().unwrap();
+        image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
+        callback.forget();
+
+        image.set_src("Idle (1).png");
+        success_rx.await;
+
+        context.draw_image_with_html_image_element(&image, 0.0, 0.0).unwrap();
+
+        sierpinski(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)], (0, 255, 0), 5);
+    });
+
 
     console::log_1(&JsValue::from_str("Hello world!"));
 
